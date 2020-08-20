5a4c12fb69b5fe210abd71a4125347e79f5a39c0
MetalcoreLoli <deadpool1118@gmail.com>
2020-08-20 22:00:41 +0300

    init commit

---
+++ xmobar/layout_change.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -n "$1" ]; then 
+    setxkbmap $1
+else 
+    layout=$(setxkbmap -query | awk 'END{print $2}')
+    case $layout in 
+        us) setxkbmap ru;;
+        *) setxkbmap us;;
+    esac
+fi
+
